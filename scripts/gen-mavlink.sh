252a9cb52985ba2d51672f283c070da585ce468b
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-02 14:20:09 +0800

    Untrack pre-generated mavlink and add generation script

---
+++ scripts/gen-mavlink.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function ASSERT
+{
+    $*
+    RES=$?
+    if [ $RES -ne 0 ]; then
+        echo 'Assert failed: "' $* '"'
+        exit $RES
+    fi
+}
+
+PASS_COLOR='\e[32;01m'
+NO_COLOR='\e[0m'
+function OK
+{
+    printf " [ ${PASS_COLOR} OK ${NO_COLOR} ]\n"
+}
+
+function gen_mavlink
+{
+    rm -rf /tmp/mavlink
+    pushd /tmp/
+    ASSERT git clone https://github.com/mavlink/mavlink.git --recursive
+    cd mavlink/
+    python3 ./pymavlink/tools/mavgen.py \
+            --lang=C --wire-protocol=2.0 \
+            --output=generated/include/mavlink/v2.0 \
+            message_definitions/v1.0/common.xml
+    popd
+    cp -r /tmp/mavlink/generated/include/mavlink/v2.0/* lib/mavlink/
+}
+
+gen_mavlink && OK
