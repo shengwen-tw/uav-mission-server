4266047c7653052203179e9e0634a2bc63905c2f
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-07 20:39:17 +0800

    Parse GPS_RAW_INT message from FCU

--- fcu.c
+++ fcu.c
@@ -5,16 +5,19 @@
 
 #define FCU_CHANNEL MAVLINK_COMM_1
 
+void mav_fcu_gps_raw_int(mavlink_message_t *recvd_msg);
 void mav_fcu_rc_channel(mavlink_message_t *recvd_msg);
 
 /* clang-format off */
 enum {
     ENUM_MAVLINK_HANDLER(mav_fcu_rc_channel),
+    ENUM_MAVLINK_HANDLER(mav_fcu_gps_raw_int),
     FCU_MAV_CMD_CNT
 };
 /* clang-format on */
 
 struct mavlink_cmd fcu_cmds[] = {
+    DEF_MAVLINK_CMD(mav_fcu_gps_raw_int, 24),
     DEF_MAVLINK_CMD(mav_fcu_rc_channel, 65),
 };
 
@@ -29,6 +32,33 @@ void fcu_read_mavlink_msg(uint8_t *buf, size_t nbytes)
             parse_mavlink_msg(&fcu_msg, fcu_cmds, FCU_MAV_CMD_CNT);
         }
     }
+}
+
+void mav_fcu_gps_raw_int(mavlink_message_t *recvd_msg)
+{
+    mavlink_gps_raw_int_t gps_raw_int;
+    mavlink_msg_gps_raw_int_decode(recvd_msg, &gps_raw_int);
+
+   /* Check: mavlink_msg_gps_raw_int.h
+    typedef struct __mavlink_gps_raw_int_t {
+        uint64_t time_usec;
+        int32_t lat;
+        int32_t lon;
+        int32_t alt;
+        uint16_t eph;
+        uint16_t epv;
+        uint16_t vel;
+        uint16_t cog;
+        uint8_t fix_type;
+        uint8_t satellites_visible;
+        int32_t alt_ellipsoid;
+        uint32_t h_acc;
+        uint32_t v_acc;
+        uint32_t vel_acc;
+        uint32_t hdg_acc;
+        uint16_t yaw;
+    } mavlink_gps_raw_int_t;
+    */
 }
 
 void mav_fcu_rc_channel(mavlink_message_t *recvd_msg)
