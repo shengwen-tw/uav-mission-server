954f707bcaf64d905efaeae8ca6b637928dcde3f
Shengwen Cheng <shengwen1997.tw@gmail.com>
2024-01-14 22:36:39 +0800

    Verify device status by checking private pointers

--- src/device.c
+++ src/device.c
@@ -8,6 +8,14 @@
 static struct camera_dev camera_devs[CAMERA_NUM_MAX];
 static int camera_cnt;
 
+static inline bool device_present(int id)
+{
+    if (camera_devs[id].camera_priv || camera_devs[id].gimbal_priv)
+        return true;
+    else
+        return false;
+}
+
 int register_camera(struct camera_operations *camera_ops)
 {
     if (camera_cnt >= CAMERA_NUM_MAX) {
@@ -24,7 +32,7 @@ int register_camera(struct camera_operations *camera_ops)
 
 void camera_open(int id, void *args)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->camera_open)
+    if (!CAMERA_OPS(id)->camera_open)
         return;
 
     CAMERA_OPS(id)->camera_open(&camera_devs[id], args);
@@ -32,7 +40,7 @@ void camera_open(int id, void *args)
 
 void camera_close(int id)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->camera_close)
+    if (!device_present(id) || !CAMERA_OPS(id)->camera_close)
         return;
 
     CAMERA_OPS(id)->camera_close(&camera_devs[id]);
@@ -40,7 +48,7 @@ void camera_close(int id)
 
 void camera_save_image(int id)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->camera_save_image)
+    if (!device_present(id) || !CAMERA_OPS(id)->camera_save_image)
         return;
 
     CAMERA_OPS(id)->camera_save_image(&camera_devs[id]);
@@ -48,7 +56,7 @@ void camera_save_image(int id)
 
 void camera_change_record_state(int id)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->camera_change_record_state)
+    if (!device_present(id) || !CAMERA_OPS(id)->camera_change_record_state)
         return;
 
     CAMERA_OPS(id)->camera_change_record_state(&camera_devs[id]);
@@ -56,7 +64,7 @@ void camera_change_record_state(int id)
 
 void camera_zoom(int id, uint8_t zoom_integer, uint8_t zoom_decimal)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->camera_zoom)
+    if (!device_present(id) || !CAMERA_OPS(id)->camera_zoom)
         return;
 
     CAMERA_OPS(id)->camera_zoom(&camera_devs[id], zoom_integer, zoom_decimal);
@@ -64,7 +72,7 @@ void camera_zoom(int id, uint8_t zoom_integer, uint8_t zoom_decimal)
 
 void gimbal_open(int id, void *args)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->gimbal_open)
+    if (!CAMERA_OPS(id)->gimbal_open)
         return;
 
     CAMERA_OPS(id)->gimbal_open(&camera_devs[id], args);
@@ -72,7 +80,7 @@ void gimbal_open(int id, void *args)
 
 void gimbal_close(int id)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->gimbal_close)
+    if (!device_present(id) || !CAMERA_OPS(id)->gimbal_close)
         return;
 
     CAMERA_OPS(id)->gimbal_close(&camera_devs[id]);
@@ -80,7 +88,7 @@ void gimbal_close(int id)
 
 void gimbal_centering(int id)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->gimbal_centering)
+    if (!device_present(id) || !CAMERA_OPS(id)->gimbal_centering)
         return;
 
     CAMERA_OPS(id)->gimbal_centering(&camera_devs[id]);
@@ -88,7 +96,7 @@ void gimbal_centering(int id)
 
 void gimbal_rotate(int id, int16_t yaw, int16_t pitch)
 {
-    if (id >= camera_cnt || !CAMERA_OPS(id)->gimbal_rotate)
+    if (!device_present(id) || !CAMERA_OPS(id)->gimbal_rotate)
         return;
 
     CAMERA_OPS(id)->gimbal_rotate(&camera_devs[id], yaw, pitch);
